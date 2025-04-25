efb90221394159d6ce3d6376f6e03cedaa4a44c1
ianmseddy <ianmseddy@gmail.com>
2025-04-25 13:17:29 -0700

    updates

--- global.R
+++ global.R
@@ -6,50 +6,50 @@ if (tryCatch(packageVersion("SpaDES.project") < "0.1.1", error = function(x) TRU
   Require::Require("stringr")
 }
 
-ecoregionName <- "Lac Seul Upland"
-studyTime <- 250
+ecoregionName <- "Horn Plateau"
+studyTime <- 100
 studyAreaName <- stringr::str_replace_all(ecoregionName, " ", "")
-# uniqueFts <- ""
-runName <- paste0(studyAreaName, studyTime
-                  # ,"_", uniqueFts
-                  )
+runName <- paste0(studyAreaName, studyTime)
 
 
 out <- SpaDES.project::setupProject(
   Restart = TRUE,
   functions = "ianmseddy/Regen_testing@main/R/customFunctions.R",
   updateRprofile = TRUE,
+  packages = c('RCurl', 'XML', 'snow', 'googledrive', "PredictiveEcology/LandR@development"),
+  useGit = TRUE,
   paths = list(projectPath = getwd(),
                inputPath = "inputs",
                outputPath = file.path("outputs", runName),
                cachePath = "cache"),
-  modules = c("PredictiveEcology/Biomass_borealDataPrep@development",
-              "PredictiveEcology/Biomass_core@development",
-              "PredictiveEcology/Biomass_regeneration@development",
-              "PredictiveEcology/Biomass_speciesParameters@development",
-              "PredictiveEcology/scfm@development"
-              ),
+  options = list(#spades.allowInitDuringSimInit = TRUE,
+    reproducible.inputPaths = "D:/Ian/Data",
+    gargle_oauth_client_type = "installed", #I think?
+    gargle_oauth_email = "ianmseddy@gmail.com",
+    spades.allowSequentialCaching = TRUE,
+    spades.moduleCodeChecks = FALSE,
+    spades.recoveryMode = 1
+  ),
+  modules = c(
+    "PredictiveEcology/Biomass_speciesData@biomassParam",
+    "PredictiveEcology/Biomass_borealDataPrep@biomassParameterizationObj",
+    "PredictiveEcology/Biomass_core@development",
+    "PredictiveEcology/Biomass_regeneration@development",
+    "PredictiveEcology/Biomass_speciesParameters@development",
+    "PredictiveEcology/scfm@development"
+  ),
               #note scfm is a series of modules on a single git repository
+  times = list(start = 2011, end = 2011 + studyTime),
   params = list(
     .globals = list(
       dataYear = 2011, #will get kNN 2011 data, and NTEMS 2011 landcover
       sppEquivCol = "LandR",
       .plots = c("png"),
       .studyAreaName = studyAreaName,
-      .useCache = c(".inputObjects", "init")
+      .useCache = c(".inputObjects")
     ),
-
     Biomass_core = list(.plotInterval = 25)
   ),
-  options = list(#spades.allowInitDuringSimInit = TRUE,
-    spades.allowSequentialCaching = TRUE,
-    spades.moduleCodeChecks = FALSE,
-    spades.recoveryMode = 1,
-    LandR.verbose = TRUE #for regen messages
-  ),
-  packages = c('RCurl', 'XML', 'snow', 'googledrive'),
-  times = list(start = 2011, end = 2011 + studyTime),
-  useGit = TRUE,
   #70 years of fire should be enough to evaluate MAAB
   studyArea = {
     targetCRS <- paste("+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95 +x_0=0 +y_0=0",
@@ -60,39 +60,29 @@ out <- SpaDES.project::setupProject(
 
     }
     ecor <- reproducible::prepInputs(url = "https://sis.agr.gc.ca/cansis/nsdb/ecostrat/region/ecoregion_shp.zip",
-                                     destinationPath = 'inputs')
+                                     destinationPath = 'inputs', fun = "terra::vect")
     ecor <- ecor[ecor$REGION_NAM == ecoregionName,]
-    ecor <- sf::st_transform(ecor, targetCRS)
-    ecor <- sf::st_buffer(ecor, 10000)
+    ecor <- terra::project(ecor, targetCRS)
+    ecor <- terra::buffer(ecor, 10000)
   },
-  studyAreaLarge = studyArea,
-  studyAreaCalibration = {
-    sf::st_buffer(studyArea, 20000)
-  },
+  studyAreaLarge = terra::buffer(studyArea, 5000),
   sppEquiv = {
-    speciesInStudy <- LandR::speciesInStudyArea(studyAreaLarge,
-                                                dPath = "inputs")
+    speciesInStudy  <- LandR::speciesInStudyArea(studyArea = studyArea, dPath = "inputs")
     species <- LandR::equivalentName(speciesInStudy$speciesList, df = LandR::sppEquivalencies_CA, "LandR")
     sppEquiv <- LandR::sppEquivalencies_CA[LandR %in% species]
     sppEquiv <- sppEquiv[KNN != "" & LANDIS_traits != ""] #avoid a bug with shore pine
   },
   rasterToMatchLarge = {
-    rtml<- terra::rast(studyAreaLarge, res = c(250, 250))
-    rtml[] <- 1
-    rtml <- terra::mask(rtml, studyAreaLarge)
+    rtml = terra::rast(studyAreaLarge, res = c(250, 250), vals = 1)
+    rtml = terra::mask(rtml, studyAreaLarge)
   },
   rasterToMatch = { #FIX THIS TO USE THE NTEMS CRS
-    rtm <- terra::crop(rasterToMatchLarge, studyArea)
-    rtm <- terra::mask(rtm, studyArea)
-    rtm
-  },
-  rasterToMatchCalibration = {
-    rtmc<- terra::rast(studyAreaCalibration, res = c(250, 250))
-    rtmc[] <- 1
-    rtmc <- terra::mask(rtmc, studyAreaCalibration)
+    rtm = reproducible::postProcess(rasterToMatchLarge, to = studyArea)
   }
 )
-
+pineUpdate = function(species){
+  species[species %in% c("Pinu_con", "Popu_tre"), shadetolerance := 1.5]
+}
 out$paths$modulePath <- c(file.path("modules"),
                           file.path("modules/scfm/modules"))
 out$modules <- c("Biomass_core", "Biomass_borealDataPrep", "Biomass_regeneration",
@@ -101,8 +91,13 @@ out$modules <- c("Biomass_core", "Biomass_borealDataPrep", "Biomass_regeneration
                  "scfmIgnition", "scfmEscape", "scfmSpread"
 )
 out$params$scfmDataPrep = list(targetN = 2000,
-                  # targetN would ideally be minimum 2000 - mean fire size estimates will be bad with 1000
-                  .useParallelFireRegimePolys = TRUE) #assumes parallelization is an option
+                               fireRegimePolysType = "FRU",
+                               .useParallelFireRegimePolys = TRUE) #assumes parallelization is an option
 out$params$scfmSpread = list(.plotInterval = 25)
+out$params$Biomass_borealDataPrep$speciesUpdateFunction = list(
+  quote(LandR::speciesTableUpdate(sim$species, sim$speciesTable,
+                                  sim$sppEquiv, P(sim)$sppEquivCol)),
+  quote(pineUpdate(sim$species)))
 
 outSim <- SpaDES.core::simInitAndSpades2(out)
+
